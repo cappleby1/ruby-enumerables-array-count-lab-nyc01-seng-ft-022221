27de4bf9d83de589ecc6f8ce6ca929d1cd400747
Colten Appleby <colten96@gmail.com>
2021-02-15 20:48:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,10 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  
+  
+  
+  return array
+  
 end
 
 def count_empty_strings(array)
